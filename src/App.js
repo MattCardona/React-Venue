06b848dc3a9c14d0d2f3430e7d82cc39ed10cbe8
Matt <boblikesbbq@gmail.com>
2018-12-09 10:57:10 -0800

    Add Featured component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header.js';
+import Featured from './components/featured/Featured.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height:"1500px", background:'red'}}>
         <Header />
+        <Featured />
       </div>
     );
   }
---
+++ src/components/featured/Featured.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Carousel from './Carousel.js';
+
+const Featured = () => {
+
+  return (
+    <div style={{position: 'relative'}}>
+      <Carousel />
+
+      <div className="artist_name">
+        <div className="wrapper">
+          Ariana Grande
+        </div>
+      </div>
+
+    </div>
+  )
+};
+
+export default Featured;
