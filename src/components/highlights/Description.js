d598a02bfff53b957dfd2e6e284d43d3ea611ec0
Matt <boblikesbbq@gmail.com>
2018-12-12 14:02:47 -0800

    Add styles to Highlight

--- src/components/highlights/Description.js
+++ src/components/highlights/Description.js
@@ -3,12 +3,14 @@ import { Fade } from 'react-reveal';
 
 const Description = () => { 
   return (
-    <div className="center_wrapper">
-      <h2>Highlights</h2>
-      <div className="highlight_description">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, adipisci nemo. Tenetur minima, veritatis illum optio magni reiciendis tempora nisi. Perferendis quidem itaque eaque dolores ipsam minus, perspiciatis a voluptatum.
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, adipisci nemo. Tenetur minima, veritatis illum optio magni reiciendis tempora nisi. Perferendis quidem itaque eaque dolores ipsam minus, perspiciatis a voluptatum.
+        </div>
       </div>
-    </div>
+    </Fade>
   )
 };
 
