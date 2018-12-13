a3d28b1f12523cf50ee9fe86c241444903112f0d
Matt <boblikesbbq@gmail.com>
2018-12-12 16:56:12 -0800

    Add animation

--- src/components/pricing/index.js
+++ src/components/pricing/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ButtonUtil from '../utils/Button.js'
+import { Zoom } from 'react-reveal';
 
 class Pricing extends Component {
   constructor(props) {
@@ -11,19 +12,22 @@ class Pricing extends Component {
           price: 150,
           positon: "Economy",
           description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptatibus eos voluptates tenetur! Quos soluta nobis repudiandae debitis, tempora",
-          link: "https://google.com"
+          link: "https://google.com",
+          delay: 510
         },
         {
           price: 250,
           positon: "Standard",
           description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptatibus eos voluptates tenetur! Quos soluta nobis repudiandae debitis, tempora",
-          link: "https://google.com"
+          link: "https://google.com",
+          delay: 0
         },
         {
           price: 450,
           positon: "VIP",
           description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptatibus eos voluptates tenetur! Quos soluta nobis repudiandae debitis, tempora",
-          link: "https://google.com"
+          link: "https://google.com",
+          delay: 510
         }
       ]
     }
@@ -32,25 +36,30 @@ class Pricing extends Component {
   showBoxes() {
     return (
         this.state.prices.map((box, i) => (
-          <div className="pricing_item">
-            <div className="pricing_inner_wrapper">
-              <div className="pricing_title">
-                <span>${box.price}</span>
-                <span>{box.positon}</span>
-              </div>
-              <div className="pricing_description">
-                <span>{box.description}</span>
-              </div>
-              <div className="pricing_buttons">
-                <ButtonUtil
-                  link={box.link}
-                  bck="#ffa800"
-                  color="#fff"
-                  text="Buy"
-                />
+          <Zoom
+            key={i}
+            delay={box.delay}
+          >
+            <div className="pricing_item">
+              <div className="pricing_inner_wrapper">
+                <div className="pricing_title">
+                  <span>${box.price}</span>
+                  <span>{box.positon}</span>
+                </div>
+                <div className="pricing_description">
+                  <span>{box.description}</span>
+                </div>
+                <div className="pricing_buttons">
+                  <ButtonUtil
+                    link={box.link}
+                    bck="#ffa800"
+                    color="#fff"
+                    text="Buy"
+                  />
+                </div>
               </div>
             </div>
-          </div>
+          </Zoom>
         )
       )
     )
