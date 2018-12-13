9d9de04bb627f9d9675337b8411403e66abd6b9a
Matt <boblikesbbq@gmail.com>
2018-12-13 13:42:19 -0800

    Updates

---
+++ precache-manifest.026a290606806c5bdc60c92564eae70b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/React-Venue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/React-Venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/React-Venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "79bc31a631d354222ea7",
+    "url": "/React-Venue/static/js/runtime~main.79bc31a6.js"
+  },
+  {
+    "revision": "cfca4867774383b0c18f",
+    "url": "/React-Venue/static/js/main.cfca4867.chunk.js"
+  },
+  {
+    "revision": "3e24526847285404c794",
+    "url": "/React-Venue/static/js/1.3e245268.chunk.js"
+  },
+  {
+    "revision": "cfca4867774383b0c18f",
+    "url": "/React-Venue/static/css/main.8ba34f71.chunk.css"
+  },
+  {
+    "revision": "699590e8ec4f1a2e3bf88810825d1973",
+    "url": "/React-Venue/index.html"
+  }
+];
